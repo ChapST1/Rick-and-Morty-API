dea0d909e2ddf8ff08261ddc0e0dc7c05a396672
Christian <1368422@senati.pe>
2022-09-13 18:45:06 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
+//variables
+const modal = document.querySelector(".modal");
+const modalCloseIcon = document.querySelector(".fa-xmark");
+
 
 let i = 1;
-async function getData(){
+async function getData() {
   const response = await fetch(`https://rickandmortyapi.com/api/character/${i}`);
   const dataJson = await response.json();
 
   const app = document.querySelector('.div');
   const appContainer = document.querySelector('.app');
-  app.innerHTML = 
+  app.innerHTML =
     `
     <article class="img-container">
         <img class="img" src="${dataJson.image}">
@@ -26,27 +30,36 @@ async function getData(){
     
     `;
   appContainer.appendChild(app);
+  const image = document.querySelectorAll('.img');
+  image.forEach(e => {
+    e.addEventListener('click', () => {
+      modal.classList.toggle("modal-active");
+      modal.firstElementChild.setAttribute('src', e.getAttribute('src'))
+
+      modalCloseIcon.addEventListener('click', () => modal.classList.remove("modal-active"))
+    })
+  })
   return appContainer;
 }
-  const btnNext = document.querySelector('.btn-next').addEventListener('click', ()=>{
-    i++;
-    getData();
-    if(i > 826){
-      i = 1;
-      getData();
-    }
-  })
-  const btnPrev = document.querySelector('.btn-prev').addEventListener('click', ()=>{
-    i--;
-    getData()
-    if(i < 1){
-      i = 826;
-      getData();
-    }
-  })
+const btnNext = document.querySelector('.btn-next').addEventListener('click', () => {
+  i++;
   getData();
-const menu = document.querySelector('.menu').addEventListener('click',()=>{
-    const menuLine = document.querySelector('.menu-line').classList.toggle('active-line')
-    const nav = document.querySelector(".nav").classList.toggle('nav-active')
+  if (i > 826) {
+    i = 1;
+    getData();
+  }
+})
+const btnPrev = document.querySelector('.btn-prev').addEventListener('click', () => {
+  i--;
+  getData()
+  if (i < 1) {
+    i = 826;
+    getData();
+  }
+})
+getData();
+const menu = document.querySelector('.menu').addEventListener('click', () => {
+  const menuLine = document.querySelector('.menu-line').classList.toggle('active-line')
+  const nav = document.querySelector(".nav").classList.toggle('nav-active')
 })
 
